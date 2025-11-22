89b6715d0f98ccd0d6490abef9852b95d82102ea
Kishor Irnak <kishor@example.com>
2025-11-22 13:04:34 +0530

    Fix GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Trending-Motion-Agency/', // CRITICAL: Matches your repository name for GH Pages
-})
+  base: "/Trending_Motion_Agency/", // 👈 Important for GitHub Pages
+});
